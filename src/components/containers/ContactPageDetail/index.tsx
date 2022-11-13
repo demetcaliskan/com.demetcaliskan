d1fe2b30c41183084396f0fb413348a09944bfbf
Demet Çalışkan <demet@aposto.com>
2022-11-13 23:16:40 +0300

    Add contact page

--- src/components/containers/ContactPageDetail/index.tsx
+++ src/components/containers/ContactPageDetail/index.tsx
@@ -1,5 +1,25 @@
+import ContactPageDetailStyles, {
+  ContactPageDetailItemText,
+  ContactPageTitle,
+  TitleDivider,
+} from './styles'
+
 const ContactPageDetail = () => {
-  return <></>
+  return (
+    <ContactPageDetailStyles>
+      <ContactPageTitle>Get In Touch</ContactPageTitle>
+      <TitleDivider />
+      <ContactPageDetailItemText>
+        You can use{' '}
+        <a href='mailTo:demetcaliskann@icloud.com'>demetcaliskann@icloud.com</a>{' '}
+        e-mail address or website links located in the header and footer to
+        reach out to me about work or any other subject.
+      </ContactPageDetailItemText>
+      <ContactPageDetailItemText>
+        Feel free to contact me :)
+      </ContactPageDetailItemText>
+    </ContactPageDetailStyles>
+  )
 }
 
 export default ContactPageDetail
--- src/components/containers/ContactPageDetail/styles.tsx
+++ src/components/containers/ContactPageDetail/styles.tsx
@@ -1,1 +1,57 @@
 import styled from 'styled-components'
+
+export default styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 1080px;
+  justify-content: center;
+  align-items: center;
+  @media (max-width: 1080px) {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+`
+
+export const TitleDivider = styled.div`
+  display: flex;
+  height: 0.5px;
+  background: ${(p) => p.theme.colors.secondary};
+  width: 100%;
+  max-width: 900px;
+  margin-bottom: 44px;
+`
+
+export const ContactPageTitle = styled.h1`
+  font-family: ${(p) => p.theme.fonts.tertiary};
+  cursor: pointer;
+  font-size: 42px;
+  line-height: 42px;
+  font-weight: 200;
+  color: #0000;
+  text-shadow: 0 0 ${(p) => p.theme.colors.secondary},
+    0 42px ${(p) => p.theme.colors.primary};
+  overflow: hidden;
+  transition: 0.3s;
+  :hover {
+    text-shadow: 0 -42px ${(p) => p.theme.colors.secondary},
+      0 0 ${(p) => p.theme.colors.primary};
+  }
+`
+
+export const ContactPageDetailItemText = styled.p`
+  font-family: ${(p) => p.theme.fonts.primary};
+  font-size: 22px;
+  line-height: 26px;
+  font-weight: 100;
+  max-width: 900px;
+  text-align: center;
+  a {
+    color: ${(p) => p.theme.colors.secondary};
+    text-shadow: none;
+    transition: 0.5s text-shadow ease;
+    :hover {
+      text-shadow: 0px 1px 12px rgba(244, 234, 241, 0.85);
+    }
+  }
+`
