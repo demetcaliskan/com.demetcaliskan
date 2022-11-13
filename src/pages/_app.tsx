83436f13ac48d061151823470690430b53e300ea
Demet Çalışkan <demet@aposto.com>
2022-11-13 06:11:58 +0300

    Change icon + Add necessary pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -13,7 +13,10 @@ export default function App({ Component, pageProps }: AppProps) {
           name='description'
           content='Personal website owned by Demet Caliskan'
         />
-        <link rel='icon' href='/favicon.ico' />
+        <link
+          rel='icon'
+          href='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👩🏻‍💻</text></svg>'
+        ></link>
       </Head>
       <BasicLayout>
         <Header />
---
+++ src/pages/works.tsx
@@ -0,0 +1,7 @@
+export default function Works() {
+  return (
+    <>
+      <p>Coming soon...</p>
+    </>
+  )
+}
