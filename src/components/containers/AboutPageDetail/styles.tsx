8d8e63458656cf4e1e96003dc3939915348950f4
Demet Çalışkan <demet@aposto.com>
2022-11-14 01:49:08 +0300

    Add about us page

--- src/components/containers/AboutPageDetail/styles.tsx
+++ src/components/containers/AboutPageDetail/styles.tsx
@@ -5,10 +5,20 @@ export default styled.div`
   flex-direction: column;
   width: 100%;
   max-width: 1080px;
+  max-width: 900px;
   justify-content: center;
   align-items: center;
   @media (max-width: 1080px) {
     padding-left: 20px;
     padding-right: 20px;
   }
+`
+
+export const AboutPageText = styled.p`
+  font-family: ${(p) => p.theme.fonts.primary};
+  font-weight: 100;
+  font-size: 16px;
+  line-height: 22px;
+  align-self: flex-start;
+  text-align: justify;
 `
